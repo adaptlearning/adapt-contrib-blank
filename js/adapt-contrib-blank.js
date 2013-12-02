01dd2cc5b6e3f5acb45b3806c0c1b64d9afe3ad9
Kevin Corry <kevinc@learningpool.com>
2013-12-02 10:40:45 +0000

    - CommonJS format for RequireJS - Adding documentation

--- js/adapt-contrib-blank.js
+++ js/adapt-contrib-blank.js
@@ -1,14 +1,17 @@
-define(["coreViews/componentView", "coreJS/adapt"], function(ComponentView, Adapt) {
+define(function(require) {
 
-    var Blank = ComponentView.extend({
-        
-        postRender: function() {
-            this.setReadyStatus();
-            this.setCompletionStatus();
-        }
-        
-    });
-    
-    Adapt.register("blank", Blank);
-    
+  var ComponentView = require('coreViews/componentView');
+  var Adapt = require('coreJS/adapt');
+
+  var Blank = ComponentView.extend({
+
+    postRender: function() {
+      this.setReadyStatus();
+      this.setCompletionStatus();
+    }
+
+  });
+
+  Adapt.register("blank", Blank);
+
 });
