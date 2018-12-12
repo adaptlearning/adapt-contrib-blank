b91e29d1fde3d5caac64c70bfd67a3c01701d8a4
Guy Willis <guy.willis@kineo.com>
2018-12-12 17:08:09 +0000

    Converted tabs to spaces, indentation amends

--- js/adapt-contrib-blank.js
+++ js/adapt-contrib-blank.js
@@ -1,30 +1,30 @@
 define([
-    'core/js/adapt',
-    'core/js/views/componentView'
+  'core/js/adapt',
+  'core/js/views/componentView'
 ], function(Adapt, ComponentView) {
 
-    var Blank = ComponentView.extend({
+  var Blank = ComponentView.extend({
 
-        preRender: function() {
-            this.$el.addClass('no-state');
-            this.checkIfResetOnRevisit();
-        },
+    preRender: function() {
+      this.$el.addClass('no-state');
+      this.checkIfResetOnRevisit();
+    },
 
-        postRender: function() {
-            this.setReadyStatus();
-            this.setupInviewCompletion();
-        },
+    postRender: function() {
+      this.setReadyStatus();
+      this.setupInviewCompletion();
+    },
 
-        checkIfResetOnRevisit: function() {
-            var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+    checkIfResetOnRevisit: function() {
+      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
 
-            if (isResetOnRevisit) {
-                this.model.reset(isResetOnRevisit);
-            }
-        }
+      if (isResetOnRevisit) {
+        this.model.reset(isResetOnRevisit);
+      }
+    }
 
-    });
+  });
 
-    return Adapt.register('blank', Blank);
+  return Adapt.register('blank', Blank);
 
 });
