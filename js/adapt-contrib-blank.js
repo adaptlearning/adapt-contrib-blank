96baac4df5db8cec659af3f92507bdf7fb70b54b
Nils Sanderson <nils@learningpool.com>
2019-03-14 17:12:00 +0000

    update to use 2 spaces for indentation

--- js/adapt-contrib-blank.js
+++ js/adapt-contrib-blank.js
@@ -1,34 +1,34 @@
 define([
-    'core/js/adapt',
-    'core/js/views/componentView',
-    'core/js/models/componentModel'
+  'core/js/adapt',
+  'core/js/views/componentView',
+  'core/js/models/componentModel'
 ], function(Adapt, ComponentView, ComponentModel) {
 
-    var BlankView = ComponentView.extend({
+  var BlankView = ComponentView.extend({
 
-        preRender: function() {
-            this.$el.addClass('no-state');
-            this.checkIfResetOnRevisit();
-        },
+    preRender: function() {
+      this.$el.addClass('no-state');
+      this.checkIfResetOnRevisit();
+    },
 
-        postRender: function() {
-            this.setReadyStatus();
-            this.setupInviewCompletion();
-        },
+    postRender: function() {
+      this.setReadyStatus();
+      this.setupInviewCompletion();
+    },
 
-        checkIfResetOnRevisit: function() {
-            var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+    checkIfResetOnRevisit: function() {
+      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
 
-            if (isResetOnRevisit) {
-                this.model.reset(isResetOnRevisit);
-            }
-        }
+      if (isResetOnRevisit) {
+        this.model.reset(isResetOnRevisit);
+      }
+    }
 
-    });
+  });
 
-    return Adapt.register('blank', {
-        model: ComponentModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
-        view: BlankView
-    });
+  return Adapt.register('blank', {
+    model: ComponentModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
+    view: BlankView
+  });
 
 });
