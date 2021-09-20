dbb74b149e476bfebe0c2154fdd2254ac717ed46
Oliver Foster <oliver.foster@kineo.com>
2021-09-20 22:00:23 +0100

    issue/11-core Clear up isResetOnRevisit

--- js/adapt-contrib-blank.js
+++ js/adapt-contrib-blank.js
@@ -6,28 +6,17 @@ define([
 
   class BlankView extends ComponentView {
 
-    preRender() {
-      this.checkIfResetOnRevisit();
-    }
-
     postRender() {
       this.setReadyStatus();
       this.setupInviewCompletion();
     }
 
-    checkIfResetOnRevisit() {
-      const isResetOnRevisit = this.model.get('_isResetOnRevisit');
-
-      if (isResetOnRevisit) {
-        this.model.reset(isResetOnRevisit);
-      }
-    }
   }
 
   BlankView.template = 'blank';
 
   return Adapt.register('blank', {
-    model: ComponentModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
+    model: ComponentModel.extend({}), // create a new class in the inheritance chain so it can be extended per component type if necessary later
     view: BlankView
   });
 
