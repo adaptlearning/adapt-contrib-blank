1be27b5283474b538cefe58a08d0c573bbfdd353
Oliver Foster <oliver.foster@kineo.com>
2022-03-24 10:55:50 +0000

    issue/2799 Switched to components.register

--- js/adapt-contrib-blank.js
+++ js/adapt-contrib-blank.js
@@ -1,8 +1,8 @@
-import Adapt from 'core/js/adapt';
+import components from 'core/js/components';
 import BlankView from './blankView';
 import ComponentModel from 'core/js/models/componentModel';
 
-export default Adapt.register('blank', {
+export default components.register('blank', {
   model: ComponentModel.extend({}), // create a new class in the inheritance chain so it can be extended per component type if necessary later
   view: BlankView
 });
